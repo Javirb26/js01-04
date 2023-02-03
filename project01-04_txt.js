8bec4a5c17bbc89512f90c305707aa197f4062f0
Javirb26 <112392832+Javirb26@users.noreply.github.com>
2023-02-03 11:23:04 -0500

    Add files via upload
    JavaScript Web for Warriors: Chapter 1: Hands on Project 4 - Shipping Address

---
+++ project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: 
+    Date:   
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
